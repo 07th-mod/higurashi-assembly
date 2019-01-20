18ec3c95774fbdc904438611c9f109638dc2c93f
Tellow Krinkle <tellowkrinkle@gmail.com>
2019-01-20 16:49:54 -0600

    Add paging to chapter jump

--- Assets.Scripts.UI.ChapterJump/ChapterJumpManager.cs
+++ Assets.Scripts.UI.ChapterJump/ChapterJumpManager.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using MOD.Scripts.UI.ChapterJump;
+using TMPro;
 
 namespace Assets.Scripts.UI.ChapterJump
 {
@@ -17,6 +18,15 @@ namespace Assets.Scripts.UI.ChapterJump
 		public List<ChapterJumpButton> JumpButtons;
 
 		public bool isActive = true;
+
+		private List<List<ChapterJumpButton>> allButtons;
+		private int currentPage = 0;
+		private int NumPages => allButtons.Count();
+		private List<ChapterJumpButton> CurrentButtons => allButtons[currentPage];
+		private UIButton PageLeft;
+		private UIButton PageRight;
+		private TextMeshPro PageNumberText;
+		private ChapterJumpButton returnButton;
 
 		private bool relayoutDone = false;
 		private static readonly float windowStartPos = 0;
@@ -33,10 +43,20 @@ namespace Assets.Scripts.UI.ChapterJump
 		private static readonly float minYPos = -330;
 		private static readonly float ySpacing = 30;
 
-		private void PopulateJumpButtonList() 
+		private void PopulateJumpButtonList()
 		{
-			// For some reason `JumpButtons` is empty on load and nothing seems to populate it
-			JumpButtons = GetComponentsInChildren<ChapterJumpButton>().Where( button => button.IsChapterButton ).ToList();
+			var childComponents = GetComponentsInChildren<ChapterJumpButton>();
+			if (allButtons == null)
+			{
+				allButtons = new List<List<ChapterJumpButton>>
+				{
+					childComponents.Where( button => button.IsChapterButton ).ToList()
+				};
+			}
+			JumpButtons = allButtons.SelectMany( x => x ).ToList();
+
+			returnButton = childComponents.Where( button => button.name == "Return" ).First();
+
 			/*
 			foreach (var button in JumpButtons)
 			{
@@ -49,10 +69,72 @@ namespace Assets.Scripts.UI.ChapterJump
 			*/
 		}
 
+		/// <summary>
+		/// Switches pages
+		/// Run with 0 movement if you change the button list to properly the buttons on the current page
+		/// </summary>
+		/// <param name="movement">The number of pages to move forward (negative will move backward)</param>
+		private void UpdatePage(int movement)
+		{
+			currentPage = Math.Max(Math.Min(currentPage + movement, NumPages - 1), 0);
+			if (PageLeft != null)
+			{
+				PageLeft.enabled = (currentPage != 0);
+				PageRight.enabled = (currentPage != NumPages - 1);
+				PageNumberText.text = (currentPage + 1) + "/" + NumPages;
+			}
+			JumpButtons.ForEach( button => button.gameObject.SetActive(false) );
+			CurrentButtons.ForEach( button => button.gameObject.SetActive(true) );
+			RelayoutButtons();
+		}
+
+		/// <summary>
+		/// Steals page button objects from the tips screen and sets them up here
+		/// </summary>
+		private void SetupPageButtons()
+		{
+			if (NumPages <= 1) { return; }
+			var gameSystem = GameSystem.Instance;
+			var tipsManager = gameSystem.TipsPrefab.GetComponent<Tips.TipsManager>();
+
+			// Steal buttons from tips manager
+			var leftButton = Instantiate(tipsManager.PageLeft);
+			leftButton.transform.SetParent(returnButton.gameObject.transform.parent, worldPositionStays: false);
+			PageLeft = leftButton.GetComponent<UIButton>();
+			var rightButton = Instantiate(tipsManager.PageRight);
+			rightButton.transform.SetParent(returnButton.gameObject.transform.parent, worldPositionStays: false);
+			PageRight = rightButton.GetComponent<UIButton>();
+			var pageNumberText = Instantiate(tipsManager.tipsPageText);
+			pageNumberText.transform.SetParent(returnButton.gameObject.transform.parent, worldPositionStays: false);
+			PageNumberText = pageNumberText.GetComponent<TextMeshPro>();
+
+			// Setup page text
+			var returnButtonPos = returnButton.transform.localPosition;
+			var returnButtonBounds = returnButton.Text.bounds;
+			PageNumberText.fontSize = returnButton.Text.fontSize;
+			PageNumberText.alignment = TextAlignmentOptions.Center;
+			PageNumberText.transform.localPosition = new Vector3(returnButtonPos.x + returnButtonBounds.center.x, returnButtonPos.y + 25, 0f);
+
+			// Setup buttons
+			PageLeft.transform.localPosition = new Vector3(20 + returnButtonPos.x + returnButtonBounds.min.x, returnButtonPos.y + 25, 0f);
+			PageRight.transform.localPosition = new Vector3(-20 + returnButtonPos.x + returnButtonBounds.max.x, returnButtonPos.y + 25, 0f);
+			PageLeft.transform.localScale = new Vector3(0.5f, 0.5f, 0);
+			PageRight.transform.localScale = new Vector3(0.5f, 0.5f, 0);
+			// With pixelSnap on, UIButton keeps resizing the buttons to have a 1:1 ratio with the sprite image
+			// which undoes the local scale we just set
+			PageLeft.pixelSnap = false;
+			PageRight.pixelSnap = false;
+
+			leftButton.GetComponent<Tips.TipsButton>().Setup( () => UpdatePage(movement: -1) );
+			rightButton.GetComponent<Tips.TipsButton>().Setup( () => UpdatePage(movement: 1) );
+			UpdatePage(movement: 0);
+		}
+
 		private void LoadJSONButtons()
 		{
 			if (MODChapterJumpController.ChapterJumpsOrNull != null)
 			{
+				var newList = new List<List<ChapterJumpButton>>();
 				var baseButton = JumpButtons[0];
 
 				// Copied from Onikakushi
@@ -81,15 +163,30 @@ namespace Assets.Scripts.UI.ChapterJump
 					position.y -= ySpacing;
 					if (position.y < minYPos)
 					{
-						position.y = 0;
-						position.x += 200; // Will be set properly by RelayoutButtons
+						if (position.x > 0)
+						{
+							position = new Vector3(0, 0, 0);
+							newList.Add(newButtons);
+							newButtons = new List<ChapterJumpButton>();
+						}
+						else
+						{
+							position.y = 0;
+							position.x += 200; // Will be set properly by RelayoutButtons
+						}
 					}
 				}
-				foreach (var old in JumpButtons)
+				newList.Add(newButtons);
+				foreach (var oldList in allButtons)
 				{
-					Destroy(old.gameObject);
+					foreach (var old in oldList)
+					{
+						Destroy(old.gameObject);
+					}
 				}
-				JumpButtons = newButtons;
+				allButtons = newList;
+				PopulateJumpButtonList();
+				UpdatePage(movement: 0);
 			}
 		}
 
@@ -97,11 +194,11 @@ namespace Assets.Scripts.UI.ChapterJump
 		private void RelayoutButtons()
 		{
 			// Every single game seems to have this in a slightly different position
-			var windowPos = JumpButtons[0].transform.parent.localPosition;
+			var windowPos = CurrentButtons[0].transform.parent.localPosition;
 			windowPos.x = -215; // What Onikakushi uses
-			JumpButtons[0].transform.parent.localPosition = windowPos;
+			CurrentButtons[0].transform.parent.localPosition = windowPos;
 
-			var activeButtons = JumpButtons.Where( button => button.isActiveAndEnabled ).ToList();
+			var activeButtons = CurrentButtons.Where( button => button.isActiveAndEnabled ).ToList();
 			activeButtons.ForEach( button => button.Text.ForceMeshUpdate() );
 
 			// Things with x values below this are considered to be on the left half, others are on the right
@@ -148,7 +245,7 @@ namespace Assets.Scripts.UI.ChapterJump
 			}
 
 			// Set the new positions into the buttons
-			foreach (var button in JumpButtons)
+			foreach (var button in CurrentButtons)
 			{
 				Vector3 posVector = button.transform.localPosition;
 				var pos = posVector.x;
@@ -192,10 +289,7 @@ namespace Assets.Scripts.UI.ChapterJump
 			{
 				UnityEngine.Object.Destroy(base.gameObject);
 			});
-			JumpButtons.ForEach(delegate(ChapterJumpButton a)
-			{
-				a.Disable();
-			});
+			JumpButtons.ForEach( button => button.Disable() );
 		}
 
 		private void SetFade(float f)
@@ -221,7 +315,7 @@ namespace Assets.Scripts.UI.ChapterJump
 			{
 				relayoutDone = true;
 				LoadJSONButtons();
-				RelayoutButtons();
+				SetupPageButtons();
 			}
 		}
 	}
