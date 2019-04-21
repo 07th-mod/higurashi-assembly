217434a293255919ab99b3ebe64643d84f565f7e
Tellow Krinkle <tellowkrinkle@gmail.com>
2019-04-20 19:32:55 -0500

    Add OutlineWidth configuration

--- Assets.Scripts.Core.Buriko/BurikoOperations.cs
+++ Assets.Scripts.Core.Buriko/BurikoOperations.cs
@@ -138,6 +138,7 @@ namespace Assets.Scripts.Core.Buriko
 		ModSetConfigFontSize,
 		ModSetChapterJumpFontSize,
 		ModSetHighestChapterFlag,
-		ModGetHighestChapterFlag
+		ModGetHighestChapterFlag,
+		ModSetMainFontOutlineWidth
 	}
 }
--- Assets.Scripts.Core.Buriko/BurikoScriptFile.cs
+++ Assets.Scripts.Core.Buriko/BurikoScriptFile.cs
@@ -2158,6 +2158,8 @@ namespace Assets.Scripts.Core.Buriko
 				return OperationMODSetHighestChapterFlag();
 			case BurikoOperations.ModGetHighestChapterFlag:
 				return OperationMODGetHighestChapterFlag();
+			case BurikoOperations.ModSetMainFontOutlineWidth:
+				return OperationMODSetMainFontOutlineWidth();
 			default:
 				ScriptError("Unhandled Operation : " + op);
 				return BurikoVariable.Null;
@@ -2535,6 +2537,15 @@ namespace Assets.Scripts.Core.Buriko
 			SetOperationType("MODGetHighestChapterFlag");
 			int key = ReadVariable().IntValue();
 			return BurikoMemory.Instance.GetHighestChapterFlag(key);
+		}
+
+		private BurikoVariable OperationMODSetMainFontOutlineWidth()
+		{
+			SetOperationType("MODSetMainFontOutlineWidth");
+			int width = ReadVariable().IntValue();
+			GameSystem.Instance.OutlineWidth = width / 100f;
+			GameSystem.Instance.MainUIController.TextWindow.outlineWidth = GameSystem.Instance.OutlineWidth;
+			return BurikoVariable.Null;
 		}
 	}
 }
