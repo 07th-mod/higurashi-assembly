fe0423ac388ed96beccb4276dd7c51d4d1269ab5
Daniel Wong <drojfjord@gmail.com>
2021-09-17 20:13:22 +1000

    Fix stuck sprite issue by directly swapping textures (See #45) - Directly swap out the texture of the character being lipsynced, rather than calling MODDrawBustshot - This should avoid issues where MODDrawBustshot is called at an innapropriate time, causing the character to get stuck on the screen. - If the MODLipSyncProcess is ever called at the wrong time, it won't ever cause the character to be stuck on the screen, however it may cause the wrong sprite to be displayed if the layer is re-used for another character immediately after. It should clear next time the sprite is re-drawn.

--- Assets.Scripts.Core.Scene/SceneController.cs
+++ Assets.Scripts.Core.Scene/SceneController.cs
@@ -905,54 +905,6 @@ namespace Assets.Scripts.Core.Scene
 		{
 		}
 
-		public void MODDrawBustshot(int layer, string textureName, Texture2D tex2d, int x, int y, int z, int oldx, int oldy, int oldz, bool move, int priority, int type, float wait, bool isblocking)
-		{
-			if (MODSkipImage(textureName))
-			{
-				return;
-			}
-
-			Layer layer2 = GetLayer(layer);
-			while (layer2.FadingOut)
-			{
-				layer2.HideLayer();
-				layer2 = GetLayer(layer);
-			}
-			if (!move)
-			{
-				oldx = x;
-				oldy = y;
-				oldz = z;
-			}
-			layer2.MODDrawLayer(textureName, tex2d, oldx, oldy, oldz, null, 1f, /*isBustshot:*/ true, type, wait, isblocking);
-			layer2.SetPriority(priority);
-			if (move)
-			{
-				layer2.MoveLayer(x, y, z, 1f, 0, wait, isblocking, adjustAlpha: false);
-			}
-			iTween.Stop(layer2.gameObject);
-			if (Mathf.Approximately(wait, 0f))
-			{
-				layer2.FinishFade();
-			}
-			else
-			{
-				layer2.FadeInLayer(wait);
-				if (isblocking)
-				{
-					GameSystem.Instance.AddWait(new Wait(wait, WaitTypes.WaitForMove, layer2.FinishFade));
-				}
-				if (layer2.UsingCrossShader() && layer2.gameObject.layer != GetActiveLayerMask())
-				{
-					SetLayerActiveOnBothScenes(layer2);
-				}
-				else
-				{
-					UpdateLayerMask(layer2, priority);
-				}
-			}
-		}
-
 		public IEnumerator MODDrawLipSync(int character, int audiolayer, string audiofile)
 		{
 			ulong coroutineId = MODSystem.instance.modSceneController.MODLipSyncInvalidateAndGenerateId(character);
@@ -982,13 +934,13 @@ namespace Assets.Scripts.Core.Scene
 						switch (exparray[k])
 						{
 						case "2":
-							MODSystem.instance.modSceneController.MODLipSyncProcess(character, "2", exp2, coroutineId);
+							MODSystem.instance.modSceneController.MODLipSyncProcess(character, exp2, coroutineId);
 							break;
 						case "1":
-							MODSystem.instance.modSceneController.MODLipSyncProcess(character, "1", exp3, coroutineId);
+							MODSystem.instance.modSceneController.MODLipSyncProcess(character, exp3, coroutineId);
 							break;
 						case "0":
-							MODSystem.instance.modSceneController.MODLipSyncProcess(character, "0", exp4, coroutineId);
+							MODSystem.instance.modSceneController.MODLipSyncProcess(character, exp4, coroutineId);
 							break;
 						}
 					}
@@ -997,9 +949,9 @@ namespace Assets.Scripts.Core.Scene
 			}
 			else
 			{
-				MODSystem.instance.modSceneController.MODLipSyncProcess(character, "0", exp4, coroutineId);
+				MODSystem.instance.modSceneController.MODLipSyncProcess(character, exp4, coroutineId);
 				yield return (object)new WaitForSeconds(0.25f);
-				MODSystem.instance.modSceneController.MODLipSyncProcess(character, "1", exp3, coroutineId);
+				MODSystem.instance.modSceneController.MODLipSyncProcess(character, exp3, coroutineId);
 				yield return (object)new WaitForSeconds(0.25f);
 				int k = 0;
 				if (GameSystem.Instance.AudioController.IsVoicePlaying(audiolayer))
@@ -1014,14 +966,14 @@ namespace Assets.Scripts.Core.Scene
 						{
 							break;
 						}
-						MODSystem.instance.modSceneController.MODLipSyncProcess(character, "0", exp4, coroutineId);
+						MODSystem.instance.modSceneController.MODLipSyncProcess(character, exp4, coroutineId);
 						yield return (object)new WaitForSeconds(0.25f);
-						MODSystem.instance.modSceneController.MODLipSyncProcess(character, "1", exp3, coroutineId);
+						MODSystem.instance.modSceneController.MODLipSyncProcess(character, exp3, coroutineId);
 						yield return (object)new WaitForSeconds(0.25f);
 					}
 				}
 			}
-			MODSystem.instance.modSceneController.MODLipSyncProcess(character, "0", exp4, coroutineId);
+			MODSystem.instance.modSceneController.MODLipSyncProcess(character, exp4, coroutineId);
 		}
 
 		public void MODLipSyncStart(int character, int audiolayer, string audiofile)
