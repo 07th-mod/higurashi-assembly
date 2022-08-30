9b0cabaded3227dc84b18091bfa4908dc591d70c
drojf <drojfjord@gmail.com>
2022-08-30 23:23:58 +1000

    Add and update mod menu folder shortcuts

--- MOD.Scripts.UI/MODMenuSupport.cs
+++ MOD.Scripts.UI/MODMenuSupport.cs
@@ -37,6 +37,7 @@ namespace MOD.Scripts.UI
 		/// </summary>
 		public static void ShowSupportButtons(Func<GUIContent, bool> buttonRenderer)
 		{
+			MODMenuCommon.Label("Save Files and Log Files");
 			{
 				GUILayout.BeginHorizontal();
 				if (buttonRenderer(new GUIContent("Show output_log.txt / Player.log",
@@ -51,13 +52,43 @@ namespace MOD.Scripts.UI
 					MODActions.ShowLogFolder();
 				}
 
-				if (buttonRenderer(new GUIContent("Show Saves", "Clearing your save files can fix some issues with game startup, and resets all mod flags.\n\n" +
+				if (buttonRenderer(new GUIContent("Show Saves", "Click to open the save folder, which includes saves, quicksaves, and global save data\n\n" +
+					"Clearing your save files can fix some issues with game startup, and resets all mod flags.\n\n" +
 					"- WARNING: Steam cloud will restore your saves if you manually delete them! Therefore, remember to disable steam cloud, otherwise your saves will magically reappear!\n" +
 					"- The 'global.dat' file stores your global unlock process and mod flags\n" +
 					"- The 'qsaveX.dat' and 'saveXXX.dat' files contain individual save files. Note that these becoming corrupted can break your game\n" +
 					"- It's recommended to take a backup of all your saves before you modify them")))
 				{
 					MODActions.ShowSaveFolder();
+				}
+
+				GUILayout.EndHorizontal();
+			}
+
+			MODMenuCommon.Label("Quick Access to Game Folders");
+			{
+				GUILayout.BeginHorizontal();
+				if (buttonRenderer(new GUIContent("Show .assets Folder", "Click to open the game's main data folder, which contains the .assets files.\n\n" +
+					"Fonts, certain textures, and other game data is kept in the sharedassets0.assets file.")))
+				{
+					Application.OpenURL(Application.dataPath);
+				}
+
+				if (buttonRenderer(new GUIContent("Show StreamingAssets Folder", "Click to open the StreamingAssets folder, which contains most of the game assets.\n\n" +
+					"- Most Images/Textures, including Sprites, Backgrounds, Text Images, and Filter effects\n" +
+					"- Voices, Background Music, and Sound Effects\n" +
+					"- Game Scripts and Compiled Game Scripts\n" +
+					"- Movies\n" +
+					"- Other Misc Game Data")))
+				{
+					Application.OpenURL(Application.streamingAssetsPath);
+				}
+				GUILayout.EndHorizontal();
+
+				GUILayout.BeginHorizontal();
+				if (buttonRenderer(new GUIContent("Show DLL Folder", "Click to open the 'Managed' folder, which contains our modded 'Assembly-CSharp.dll'")))
+				{
+					Application.OpenURL(System.IO.Path.Combine(Application.dataPath, "Managed"));
 				}
 
 				if (buttonRenderer(new GUIContent("Show Compiled Scripts", "Sometimes out-of-date scripts can cause the game to fail to start up (stuck on black screen).\n\n" +
@@ -67,10 +98,10 @@ namespace MOD.Scripts.UI
 					MODActions.ShowCompiledScripts();
 				}
 
-
 				GUILayout.EndHorizontal();
 			}
 
+			MODMenuCommon.Label("Support Pages");
 			if (buttonRenderer(new GUIContent("Open Support Page: 07th-mod.com/wiki/Higurashi/support", "If you have problems with the game, the information on this site may help.\n\n" +
 				"There are also instructions on reporting bugs, as well as a link to our Discord server to contact us directly")))
 			{
