802f4a38575d69473717ac51fbbacf67985459bf
Daniel Wong <drojfjord@gmail.com>
2022-10-29 22:14:55 +1100

    "Currently playing BGM" feature (#92)
    This PR adds a feature which displays the name and filename of the currently playing BGM.
    
    The info about the BGM is loaded from a `bgmInfo.json` file, located inside the corresponding BGM folder.
    
    * Add basic display of current BGM on right click menu
    
    * Load BGM info from JSON
    
    * Reposition BGM info button and width
    
    * Update class to match new JSON format
    
    * Fix bgm info and F10 menu overlapping
    
    * Add support for displaying multiple BGM
    
    * Make overlay size not tied to content
    
    * Improve BGM Info appearance and add utility buttons
    
    * Properly show bgm in folder on Windows, improve button appearance
    
    * Simplify overlay rendering fn
    
    * Simplify config menu button fn
    
    * Remove unused var and fix indentation

---
+++ MOD.Scripts.Core.Audio/MODBGMInfo.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using MOD.Scripts.UI;
+using Assets.Scripts.Core;
+
+namespace MOD.Scripts.Core
+{
+	public class BGMInfo
+	{
+		/// <summary>
+		/// A comment to store misc info about the BGM (any errata, or changes made to the BGM)
+		/// </summary>
+		public string comment;
+		/// <summary>
+		/// The English/Japanese name of the song
+		/// </summary>
+		public string name;
+		public string source;
+		public string url;
+
+		public BGMInfo()
+		{
+			comment = "";
+			name = "";
+			source = "";
+			url = "";
+		}
+	}
+
+	public class BGMInfoDict
+	{
+		public Dictionary<string, BGMInfo> bgmList;
+
+		public BGMInfoDict()
+		{
+			bgmList = new Dictionary<string, BGMInfo>();
+		}
+	}
+
+	public class MODBGMInfo
+	{
+		/// <summary>
+		/// Dictionary of (filepath without extension relative to streamingassets) -> BGMInfo
+		/// </summary>
+		private static Dictionary<string, BGMInfo> bgmDictionary;
+		private static Dictionary<string, bool> loadedFolders;
+
+		static MODBGMInfo()
+		{
+			bgmDictionary = new Dictionary<string, BGMInfo>();
+			loadedFolders = new Dictionary<string, bool>();
+		}
+
+		public static BGMInfo GetBGMName(string streamingAssetsRelativePath)
+		{
+			string pathWithoutExtension = Path.Combine(Path.GetDirectoryName(streamingAssetsRelativePath), Path.GetFileNameWithoutExtension(streamingAssetsRelativePath));
+			if(bgmDictionary.TryGetValue(pathWithoutExtension, out BGMInfo info))
+			{
+				return info;
+			}
+			else
+			{
+				var defaultInfo = new BGMInfo();
+				info.name = GameSystem.Instance.ChooseJapaneseEnglish("不明 BGM", "Unknown BGM");
+				return info;
+			}
+		}
+
+		// Load the bgm information from the bgmInfo.json in a given BGM Folder (located in the StreamingAssets folder)
+		public static void LoadFromJSON(string BGMFolder)
+		{
+			// Skip already loaded folders
+			if(loadedFolders.ContainsKey(BGMFolder))
+			{
+				return;
+			}
+			loadedFolders[BGMFolder] = true;
+
+			string path = Path.Combine(Application.streamingAssetsPath, Path.Combine(BGMFolder, "bgmInfo.json"));
+
+			if (!File.Exists(path))
+			{
+				Debug.Log($"MODBGMInfo(): No bgmInfo.json at [{path}] - BGM will just show as filenames");
+				return;
+			}
+
+			try
+			{
+				using (var reader = new JsonTextReader(new StreamReader(path)))
+				{
+					BGMInfoDict bgmInfoDict = JsonSerializer.Create(new JsonSerializerSettings()).Deserialize<BGMInfoDict>(reader);
+					foreach(KeyValuePair<string, BGMInfo> kvp in bgmInfoDict.bgmList)
+					{
+						// Allow missing comment in JSON
+						if(kvp.Value.comment == null)
+						{
+							kvp.Value.comment = "";
+						}
+
+						string relativePathWithoutExtension = Path.Combine(BGMFolder, kvp.Key);
+						bgmDictionary[relativePathWithoutExtension] = kvp.Value;
+					}
+				}
+			}
+			catch (System.Exception e)
+			{
+				Debug.LogError($"MODBGMInfo(): Failed to read bgm info file at [{path}]: {e.Message}");
+				MODToaster.Show("bgmInfo.json fail - check logs");
+			}
+		}
+	}
+}
