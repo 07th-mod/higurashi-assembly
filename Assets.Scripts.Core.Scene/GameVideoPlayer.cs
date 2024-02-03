fd85ab084befba1c934374b0fd1ae687bbfeb01e
drojf <1249449+drojf@users.noreply.github.com>
2024-02-04 10:39:15 +1100

    Fix video playback resolution - Previously the video playback logic was much more complicated, but I replaced the Unity example code (more or less) and it seems to work. - See https://docs.unity3d.com/ScriptReference/Video.VideoPlayer.html

--- Assets.Scripts.Core.Scene/GameVideoPlayer.cs
+++ Assets.Scripts.Core.Scene/GameVideoPlayer.cs
@@ -9,18 +9,6 @@ namespace Assets.Scripts.Core.Scene
 	{
 		private VideoPlayer player;
 
-		private Mesh mesh;
-
-		private MeshFilter meshFilter;
-
-		private MeshRenderer meshRenderer;
-
-		private Material material;
-
-		private RenderTexture renderTexture;
-
-		private const string shaderDefaultName = "MGShader/LayerShader";
-
 		private bool isActive;
 
 		public static GameVideoPlayer CreateVideoPlayer(GameObject parent, string videoPath, Vector2Int size)
@@ -35,35 +23,23 @@ namespace Assets.Scripts.Core.Scene
 			return gameVideoPlayer;
 		}
 
-		// Rounds a float to the nearest even number (multiple of 2), then converts to an int
-		private int RoundNearestEven(float value)
-		{
-			return Mathf.RoundToInt(Mathf.Round(value / 2f) * 2f);
-		}
-
+		// Note: In the vanilla game, the PlayVideo(path, video_size_x, video_size_y) takes two extra arguments (which I think is the resolution the video should play at)
+		// However after mod changes, these are no longer used, and can be set to 0.
+		// Note: Previously the video playback logic was much more complicated, but I replaced the Unity example code (more or less) and it seems to work.
+		// See https://docs.unity3d.com/ScriptReference/Video.VideoPlayer.html
 		public void Initialize(string path, Vector2Int size)
 		{
-			// NOTE: the video will be drawn ontop of whatever is already on the screen
-			// so you should add a DrawScene( "black", 500 ); just before the movie is played.
-			float screen_aspect = GameSystem.Instance.AspectRatio;
-			int screen_width = RoundNearestEven(screen_aspect * 480f);
-			int screen_height = 480;
+			// Note: Unity recommends using Camera.main instead of Camera.current, however I could only get this to work with Camera.current
+			GameObject camera = Camera.current.gameObject;
 
-			meshFilter = base.gameObject.AddComponent<MeshFilter>();
-			meshRenderer = base.gameObject.AddComponent<MeshRenderer>();
-			material = new Material(Shader.Find("MGShader/LayerShader"));
-			meshRenderer.material = material;
-			meshFilter.mesh = MGHelper.CreateMesh(screen_width, screen_height, LayerAlignment.AlignCenter, false, 0);
-			renderTexture = new RenderTexture(screen_width, screen_height, 32);
-			material.SetTexture("_Primary", renderTexture);
-			player = base.gameObject.AddComponent<VideoPlayer>();
-			player.renderMode = VideoRenderMode.RenderTexture;
-			player.targetTexture = renderTexture;
+			player = camera.AddComponent<VideoPlayer>();
+			player.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
 			player.url = path;
 			player.isLooping = false;
 			player.aspectRatio = VideoAspectRatio.FitInside;
 			player.SetDirectAudioVolume(0, GameSystem.Instance.AudioController.GetVolumeByType(Assets.Scripts.Core.Audio.AudioType.BGM) * (25f / 32f));
 			player.loopPointReached += OnFinish;
+
 			GameSystem.Instance.AddWait(new Wait(0f, WaitTypes.WaitForInput, EndPlayback));
 			isActive = true;
 		}
@@ -71,6 +47,9 @@ namespace Assets.Scripts.Core.Scene
 		public void EndPlayback()
 		{
 			isActive = false;
+			// In the vanilla game, the video player ('player') was attached to the base game object, so it would be destroyed automatically
+			// However now that the video player is attached to the camera, we need to delete it manually
+			Object.Destroy(player);
 			Object.Destroy(base.gameObject);
 		}
 
