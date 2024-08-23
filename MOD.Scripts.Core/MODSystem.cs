3c20a7d09e50dd329de9d01cd1bdcb0158e6708b
drojf <1249449+drojf@users.noreply.github.com>
2024-08-23 21:52:08 +1000

    Add proper Wine/Proton detection

--- MOD.Scripts.Core/MODSystem.cs
+++ MOD.Scripts.Core/MODSystem.cs
@@ -6,10 +6,16 @@ using MOD.Scripts.UI.Tips;
 using UnityEngine;
 using System.IO;
 
+using System.Runtime.InteropServices;
+using System;
+
 namespace MOD.Scripts.Core
 {
 	public class MODSystem
 	{
+		[DllImport("ntdll.dll")]
+		private static extern string wine_get_version();
+
 		public readonly MODMainUIController modMainUIController = new MODMainUIController();
 
 		public readonly MODSceneController modSceneController = new MODSceneController();
@@ -30,6 +36,52 @@ namespace MOD.Scripts.Core
 		public readonly MODTipsController modTipsController = fixedMODTipsControllerInstance;
 
 		private static readonly MODTipsController fixedMODTipsControllerInstance = new MODTipsController();
+
+		// Wine detection occurs once when this class is first instantiated
+		public readonly bool IsWine = false;
+
+		private bool DetectWine(out string wineVersionString)
+		{
+			if (Application.platform != RuntimePlatform.WindowsPlayer)
+			{
+				// If Unity does not report OS as Windows, we can assume we are not running Wine/Proton,
+				// as Wine should trick Unity that it is running under Windows
+				wineVersionString = null;
+				return false;
+			}
+
+			try
+			{
+				// If we can call the wine_get_version() function, then we are running under Wine/Proton
+				wineVersionString = wine_get_version();
+				return wineVersionString != null;
+			}
+			catch(EntryPointNotFoundException)
+			{
+				// If the function is not found, we are running under Windows
+				wineVersionString = null;
+				return false;
+			}
+			catch (Exception ex)
+			{
+				Debug.Log($"IsWine() Exception - {ex}");
+				wineVersionString = null;
+				return false;
+			}
+		}
+
+		public MODSystem()
+		{
+			if (DetectWine(out string wineVersionString))
+			{
+				IsWine = true;
+				Debug.Log($"WINE: Wine/Proton Detected, Version {wineVersionString} under {SystemInfo.operatingSystem}");
+			}
+			else
+			{
+				Debug.Log($"WINE: No Wine/Proton, running regular Windows version {SystemInfo.operatingSystem}");
+			}
+		}
 
 		public static string BaseDirectory
 		{
