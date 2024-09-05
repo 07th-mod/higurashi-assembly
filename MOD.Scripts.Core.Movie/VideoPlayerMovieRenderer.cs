528f06e90222edff9668dbaa3379af2bf32dbc31
drojf <1249449+drojf@users.noreply.github.com>
2024-09-05 21:38:55 +1000

    [Proton/Wine] Add workaround for movie playback not working by playing .ogv movie file if Proton/Wine detected

--- MOD.Scripts.Core.Movie/VideoPlayerMovieRenderer.cs
+++ MOD.Scripts.Core.Movie/VideoPlayerMovieRenderer.cs
@@ -30,7 +30,10 @@ namespace MOD.Scripts.Core.Movie
 		public bool GetFullVideoPath(string pathNoExtension, out string pathWithExtension)
 		{
 			string[] extensionList = WindowsExtensionsList;
-			if (Application.platform != RuntimePlatform.WindowsPlayer)
+			// For now, assume Proton/Wine does not support .mp4 playback. See:
+			// - https://github.com/07th-mod/hou-plus/issues/12
+			// - https://github.com/07th-mod/higurashi-assembly/commit/67746c80b14b795a0a9cdd8caa06d651519e28bc
+			if (Application.platform != RuntimePlatform.WindowsPlayer || MODSystem.instance.IsWine)
 			{
 				extensionList = MacLinuxExtensionsList;
 			}
