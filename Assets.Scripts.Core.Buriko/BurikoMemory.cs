14d27ad049f56e78d8db5e462afd9b18597ef29d
drojf <1249449+drojf@users.noreply.github.com>
2025-10-25 15:24:18 +1100

    Add letterboxed scaling for ryukishi 4:3 mode based on GRyukishiMode43Letterbox flag

--- Assets.Scripts.Core.Buriko/BurikoMemory.cs
+++ Assets.Scripts.Core.Buriko/BurikoMemory.cs
@@ -117,6 +117,7 @@ namespace Assets.Scripts.Core.Buriko
 			variableReference.Add("GBackgroundSet", 528);
 			variableReference.Add("GAudioSet", 529);
 			variableReference.Add("GRyukishiMode43Aspect", 530);
+			variableReference.Add("GRyukishiMode43Letterbox", 531);
 
 			// 611 - 619 used for additional chapter progress info
 			SetGlobalFlag("GMessageSpeed", 60);
