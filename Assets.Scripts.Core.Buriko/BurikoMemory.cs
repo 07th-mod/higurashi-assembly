5608c03ede2229f22f91eb1ad76e15a8e073522d
drojf <1249449+drojf@users.noreply.github.com>
2025-11-09 22:42:14 +1100

    Restore global.dat backup if last write was interrupted - Because we write to the `global.dat` more often (see https://github.com/07th-mod/higurashi-assembly/pull/144) and `global.dat` will kind of wipe your progress if it is missing, the below logic is added to restore a backup if the WriteAllBytesSemiAtomic() was interrupted causing `global.dat` to be missing.

--- Assets.Scripts.Core.Buriko/BurikoMemory.cs
+++ Assets.Scripts.Core.Buriko/BurikoMemory.cs
@@ -522,6 +522,19 @@ namespace Assets.Scripts.Core.Buriko
 		public void LoadGlobals()
 		{
 			string path = Path.Combine(MGHelper.GetSavePath(), "global.dat");
+
+			// Because we write to the `global.dat` more often (see https://github.com/07th-mod/higurashi-assembly/pull/144)
+			// and `global.dat` will kind of wipe your progress if it is missing, the below logic is added
+			// to restore a backup if the WriteAllBytesSemiAtomic() was interrupted causing `global.dat` to be missing.
+			//
+			// The old file would be at `global.dat.temporarybackup`
+			if(MODUtilityNoDeps.RestoreSemiAtomicBackupIfRequired(path))
+			{
+				string message = "Warning: Restored backup global.dat";
+				MODToaster.Show(message, toastDuration: 10);
+				Logger.LogWarning(message);
+			}
+
 			if (!File.Exists(path))
 			{
 				SetGlobalFlag("GUsePrompts", 1);
--- MOD.Scripts.Core/MODUtilityNoDeps.cs
+++ MOD.Scripts.Core/MODUtilityNoDeps.cs
@@ -24,6 +24,35 @@ public static class MODUtilityNoDeps
 		File.Move(src, dst);
 	}
 
+	// Restores a backup (from WriteAllBytesSemiAtomic()) if 'path' doesn't exist
+	// If you passed a custom backupExt to WriteAllBytesSemiAtomic(), then you must use the same
+	// backupExt so the backup file can be found.
+	//
+	// Note: It would be possible to "complete" the transaction if we knew the temporary file was
+	// fully written, but not yet moved, but the complexity/chance/benefit tradeoff is not worth it
+	// for this application, as the files we are moving are very small.
+	static public bool RestoreSemiAtomicBackupIfRequired(string path, string backupExt = ".temporarybackup")
+	{
+		string backupPath = path + backupExt;
+
+		// If the target file already exists, take no action
+		if (File.Exists(path))
+		{
+			return false;
+		}
+
+		// If there is no backup to restore, take no action
+		if (!File.Exists(backupPath))
+		{
+			return false;
+		}
+
+		// If the target file doesn't exist, and there is a backup, restore the backup
+		File.Move(backupPath, path);
+
+		return true;
+	}
+
 	// Try to write to a new or existing file atomically, such that the final file is never half-written.
 	// There is also File.Replace, but I'm not sure if it works the way I'd expect, so I have written my own function below.
 	// I also avoid using Path.GetTempFileName as sometimes can have problems moving files if they are on different drives on Windows
